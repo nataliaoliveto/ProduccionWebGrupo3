c3a754a8123edc6262a2f706c03699bcf4dd15c8
fgalvarez7 <63889067+fgalvarez7@users.noreply.github.com>
2020-11-11 15:22:49 -0300

    Update ProduccionWebTP1.sql

--- ProduccionWebTP1.sql
+++ ProduccionWebTP1.sql
@@ -299,3 +299,243 @@ COMMIT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 05-05-2018 a las 03:51:28
+-- Versión del servidor: 10.1.19-MariaDB
+-- Versión de PHP: 5.6.24
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `produccionweb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `perfil`
+--
+
+CREATE TABLE `perfil` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `perfil`
+--
+
+INSERT INTO `perfil` (`id`, `nombre`) VALUES
+(1, 'Administrador'),
+(2, 'Ventas'),
+(3, 'Entregas'),
+(10, ' test2');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `perfil_permisos`
+--
+
+CREATE TABLE `perfil_permisos` (
+  `id` int(11) NOT NULL,
+  `perfil_id` int(11) NOT NULL,
+  `permiso_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `perfil_permisos`
+--
+
+INSERT INTO `perfil_permisos` (`id`, `perfil_id`, `permiso_id`) VALUES
+(1, 9, 2),
+(2, 9, 3),
+(3, 9, 4),
+(19, 10, 1),
+(20, 10, 4),
+(29, 1, 1),
+(30, 1, 2),
+(31, 1, 4);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `permisos`
+--
+
+CREATE TABLE `permisos` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(250) NOT NULL,
+  `cod` varchar(40) NOT NULL,
+  `seccion` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `permisos`
+--
+
+INSERT INTO `permisos` (`id`, `nombre`, `cod`, `seccion`) VALUES
+(1, 'Agregar usuarios', 'user.add', 'user'),
+(2, 'Modificar usuarios', 'user.edit', 'user'),
+(3, 'Borrar Usuarios', 'user.del', 'user'),
+(4, 'Ver Usuarios', 'user.see', 'user'),
+(5, 'Agregar Noticias', 'news.add', 'news');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id_usuario` int(11) NOT NULL,
+  `nombre` varchar(100) NOT NULL,
+  `apellido` varchar(100) NOT NULL,
+  `usuario` varchar(20) NOT NULL,
+  `clave` varchar(50) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `tipo` int(2) NOT NULL,
+  `activo` tinyint(1) NOT NULL,
+  `salt` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `usuarios`
+--
+
+INSERT INTO `usuarios` (`id_usuario`, `nombre`, `apellido`, `usuario`, `clave`, `email`, `tipo`, `activo`, `salt`) VALUES 
+(1, 'Admin', 'Sistema', 'admin', '207acd61a3c1bd506d7e9a4535359f8a', 'admin@carrito.com', 1, 1, 'salt');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios_perfiles`
+--
+
+CREATE TABLE `usuarios_perfiles` (
+  `id` int(11) NOT NULL,
+  `usuario_id` int(11) NOT NULL,
+  `perfil_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `usuarios_perfiles`
+--
+
+INSERT INTO `usuarios_perfiles` (`id`, `usuario_id`, `perfil_id`) VALUES
+(1, 1, 1),
+(2, 1, 2),
+(3, 1, 3),
+(4, 1, 4),
+(5, 1, 5);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios_tipos`
+--
+
+CREATE TABLE `usuarios_tipos` (
+  `id_tipo` int(11) NOT NULL,
+  `tipo` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `usuarios_tipos`
+--
+
+INSERT INTO `usuarios_tipos` (`id_tipo`, `tipo`) VALUES
+(1, 'admin'),
+(2, 'ventas'),
+(3, 'entregas');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `perfil`
+--
+ALTER TABLE `perfil`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `perfil_permisos`
+--
+ALTER TABLE `perfil_permisos`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `permisos`
+--
+ALTER TABLE `permisos`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id_usuario`);
+
+--
+-- Indices de la tabla `usuarios_perfiles`
+--
+ALTER TABLE `usuarios_perfiles`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `usuarios_tipos`
+--
+ALTER TABLE `usuarios_tipos`
+  ADD PRIMARY KEY (`id_tipo`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `perfil`
+--
+ALTER TABLE `perfil`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+--
+-- AUTO_INCREMENT de la tabla `perfil_permisos`
+--
+ALTER TABLE `perfil_permisos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
+--
+-- AUTO_INCREMENT de la tabla `permisos`
+--
+ALTER TABLE `permisos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+--
+-- AUTO_INCREMENT de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+--
+-- AUTO_INCREMENT de la tabla `usuarios_perfiles`
+--
+ALTER TABLE `usuarios_perfiles`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+--
+-- AUTO_INCREMENT de la tabla `usuarios_tipos`
+--
+ALTER TABLE `usuarios_tipos`
+  MODIFY `id_tipo` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
+
